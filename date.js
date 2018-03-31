33dc1402afe1a66a23c382137dfb132759107a0a
Luca Ramundo <luca@ramundo.info>
2018-03-31 14:06:39 +0200

    Refine date manipulation API

--- date.js
+++ date.js
@@ -1,20 +1,39 @@
 const moment = require("moment")
+const pipe = require("./pipe")
 
-const init = date =>
-  !!date ? moment(date) : moment()
+const setTime = (hours, minutes, seconds) => date =>
+  date
+    .hours(hours)
+    .minutes(minutes)
+    .seconds(seconds)
 
-const days = (date, days) =>
+const shiftDays = days => date =>
   date.add(days, "days")
 
-const time = (date, hours, minutes) =>
-  date.hours(hours).minutes(minutes)
+const parseTime = time =>
+  time.split(":")
+
+const DATE_FORMAT = "Y-MM-D"
+const today = () =>
+  moment().format(DATE_FORMAT)
+
+const TIME_FORMAT = "HH:mm:ss"
+const now = () =>
+  moment().format(TIME_FORMAT)
 
 const format = date =>
-  date.format("Y-MM-DTHH:mm:ss ZZ")
+  date.format(`${DATE_FORMAT}T${TIME_FORMAT} ZZ`)
+
+const compute = (date, time, days) =>
+  pipe(
+    date,
+    setTime(...parseTime(time)),
+    shiftDays(days)
+  )
 
 module.exports = {
-  init,
-  days,
-  time,
-  format
+  format,
+  today,
+  now,
+  compute
 }
